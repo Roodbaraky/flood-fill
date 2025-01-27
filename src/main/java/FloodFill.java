29f0ae6ac005834311d7036e196c9f8562cc5812
Roodbaraky <kooroshr@hotmail.com>
2025-01-27 11:02:58 +0000

    Initial commit

---
+++ src/main/java/FloodFill.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class FloodFill {
+    public static void main(String[] args) {
+        System.out.println("Working");
+
+        JFrame frame = new JFrame("Flood Fill");
+        frame.setVisible(true);
+        frame.setSize(600, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
